5d0235a4d6e27392b7e1b23b72aaecd07a7d41d4
Alex Sweeney <ajsweeney2324@gmail.com>
2024-11-03 19:51:08 -0600

    adding photos

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('', home, name='home'),  
-    path("recipes/", include("recipes.urls")),
+    # path("recipes/", include("recipes.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
