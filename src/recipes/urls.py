d6d23452507b17ade2f915c3ce6cffea227a24e4
Alex Sweeney <ajsweeney2324@gmail.com>
2024-11-04 18:35:28 -0600

    recipe details page started, photo not showing up yet

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import recipes
+from .views import recipes, recipe_detail
 
 urlpatterns = [
     path('', recipes, name='recipes'),
+    path('<int:id>/', recipe_detail, name='recipe_detail'),
 ]
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
-# Create your views here.
 def home(request):
    return render(request, 'recipes/home.html')
 
 def recipes(request):
-    return render(request, 'recipes/recipes.html')
+    all_recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipes.html', {'recipes': all_recipes})
+
+def recipe_detail(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
+
