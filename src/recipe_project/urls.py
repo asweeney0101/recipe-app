ba824676517934eb93272a4516826ee53b16adf1
Alex Sweeney <ajsweeney2324@gmail.com>
2024-11-07 13:30:22 -0600

    login view added

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from recipes.views import home
 from django.conf import settings
 from django.conf.urls.static import static
+from recipes.views import home
+from .views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('recipes/', include('recipes.urls')),
+    path('login/', login_view, name='login'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
