a94c407903290489db68d2eca94728f13fd519ae
Alex Sweeney <ajsweeney2324@gmail.com>
2024-11-09 12:51:11 -0600

    msg

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -13,4 +13,4 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=se
