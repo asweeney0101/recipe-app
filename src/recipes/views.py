1f36a2c06fa482f2b87bf773c3dbc49d6e3ec721
Alex Sweeney <ajsweeney2324@gmail.com>
2024-11-13 16:39:56 -0600

    steps 1-2

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
+from .forms import RecipesSearchForm
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'recipes/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = RecipesSearchForm()
+        return context
 
 class RecipesListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipes/recipes.html'
     context_object_name = 'recipes'
+
 
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
